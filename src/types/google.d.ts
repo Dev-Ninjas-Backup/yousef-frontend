e5d77bad80ede86b560451f9ba06567842f8cd90
Masud <masudparvez2050@gmail.com>
2026-01-08 04:32:52 +0600

    garage create and edit form update add address and all location info details

--- src/types/google.d.ts
+++ src/types/google.d.ts
@@ -10,6 +10,43 @@ declare global {
           prompt: () => void;
         };
       };
+      maps: {
+        places: {
+          Autocomplete: new (
+            input: HTMLInputElement,
+            options?: {
+              componentRestrictions?: { country: string };
+              fields?: string[];
+              types?: string[];
+            }
+          ) => google.maps.places.Autocomplete;
+        };
+        event?: {
+          clearInstanceListeners: (instance: any) => void;
+        };
+      };
+    };
+  }
+}
+
+declare namespace google.maps.places {
+  interface Autocomplete {
+    addListener(eventName: string, handler: () => void): void;
+    getPlace(): {
+      address_components?: {
+        long_name: string;
+        short_name: string;
+        types: string[];
+      }[];
+      formatted_address?: string;
+      geometry?: {
+        location?: {
+          lat(): number;
+          lng(): number;
+        };
+      };
+      name?: string;
+      place_id?: string;
     };
   }
 }
